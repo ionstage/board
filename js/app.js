cdddbd940525010133ec0502648a5e8b7c3a5337
nasitra <nasitra@users.noreply.github.com>
2020-12-27 08:53:10 +0900

    Create 'main' class

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
+import { Main } from './main.js';
+
 class App {
+  constructor() {
+    this.main = new Main({ element: document.querySelector('.main') });
+  }
+
   load() { /* TODO */ }
 }
 
---
+++ js/main.js
@@ -0,0 +1,5 @@
+export class Main {
+  constructor(props) {
+    this.element = props.element;
+  }
+}

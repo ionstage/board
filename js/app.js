e3f71f5dd5a5dedaafe215a681a274fcf4d150aa
nasitra <nasitra@users.noreply.github.com>
2022-02-08 18:52:48 +0900

    Modify code to use ES modules

--- js/app.js
+++ js/app.js
@@ -1,24 +1,3 @@
-(function() {
-  'use strict';
-
-  var Main = function(props) {
-    this.element = props.element;
-  };
-
-  var App = function() {
-    this.main = new Main({ element: document.querySelector('.main') });
-  };
-
-  App.prototype.load = function() { /* TODO */ };
-
-  var main = function() {
-    var app = new App();
-    app.load();
-
-    if (location.hostname === 'localhost') {
-      window.app = app;
-    }
-  };
-
-  main();
-})();
+export class App {
+  init() { /* TODO */ }
+}
